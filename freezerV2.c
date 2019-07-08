11453eb15bcb028076de52329b9549887669555c
Daniel Lim <daniellimzijie@gmail.com>
2019-07-08 08:33:30 +0000

    freezerv2.c completed

--- freezerV2.c
+++ freezerV2.c
@@ -8,6 +8,6 @@ int main (void)
     printf("Time since power failure in hours and minutes: \n");
     scanf("%d %f", &hours, &minutes);
     float t = hours + (minutes / 60);
-    float temp = (4 * (pow(t, 2)))/(t + 2) - 20;
+    float temp = (4 * (pow(t, 10)))/((pow(t , 9)) + 2) - 20;
     printf("Temperature in freeer =  %.2f\n", temp);
 }
