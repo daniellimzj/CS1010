7f5495572c6f3ef29019ac6dce66ee0ac6f058a4
Daniel Lim <daniellimzijie@gmail.com>
2019-07-08 08:10:29 +0000

    Testing out Git

--- freezer.c
+++ freezer.c
@@ -2,6 +2,10 @@
 
 int main (void)
 {
-    printf("Time since power failure in hours and minutes: \n")
-    scanf(""%d %d", hours,minutes)
+    int hours = 0;
+    int minutes = 0;
+    printf("Time since power failure in hours and minutes: \n");
+    scanf("%d %d", &hours, &minutes);
+    printf("%d %d\n", hours, minutes);
+    
 }
