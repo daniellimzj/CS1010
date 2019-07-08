9a3889c65d1f63087e09d530f52e36ced4a88550
Daniel Lim <daniellimzijie@gmail.com>
2019-07-08 08:31:57 +0000

    freezer.c completed

--- freezer.c
+++ freezer.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <math.h>
 
 int main (void)
 {
     int hours = 0;
-    int minutes = 0;
+    float minutes = 0;
     printf("Time since power failure in hours and minutes: \n");
-    scanf("%d %d", &hours, &minutes);
-    printf("%d %d\n", hours, minutes);
-    
+    scanf("%d %f", &hours, &minutes);
+    float t = hours + (minutes / 60);
+    float temp = (4 * (pow(t, 2)))/(t + 2) - 20;
+    printf("Temperature in freeer =  %.2f\n", temp);
 }
